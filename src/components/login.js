abcf310c4da3a15c6ec3607f48c99f097a6ab854
Amarbold <amarbold589@gmail.com>
2022-08-17 10:36:44 +0800

    first commit

---
+++ src/components/login.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import "./login.css"
+
+const Login = () => {
+
+    const [popupStyle, showPopup] = useState("hide")
+
+    const popup = () => {
+        showPopup("login-popup")
+        setTimeout(() => showPopup("hide"), 3000)
+    }
+
+    return (
+        <div className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+
+            <div className="login-btn" onClick={popup}>Login</div>
+
+            <p className="text">Or login using</p>
+
+            <div className="alt-login">
+                <div className="google"></div>
+            </div>
+
+            <div className={popupStyle}>
+                <h3>Login Failed</h3>
+                <p>Username or password incorrect</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
